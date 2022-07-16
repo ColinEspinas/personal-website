2b662f3bd04939c1fa562868d7bf26760b6abfc6
ColinEspinas <colin.espinas@yahoo.fr>
2022-07-16 22:29:37 +0200

    feat: added sitemap generation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,16 @@
 import { defineConfig } from 'astro/config';
 import vue from "@astrojs/vue";
-
 import tailwind from "@astrojs/tailwind";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  site: 'https://colinespinas.com',
+  integrations: [tailwind(), sitemap()],
   vite: {
     ssr: {
-      external: ["svgo"],
-    },
-  },
+      external: ["svgo"]
+    }
+  }
 });
