dd9ac4960b43f8808bc82b7a0169a67e044c9e8a
ColinEspinas <colin.espinas@yahoo.fr>
2022-07-25 16:14:02 +0200

    update: changed navigation active animation

--- src/scripts/navigation.ts
+++ src/scripts/navigation.ts
@@ -1,11 +1,16 @@
 import swup from './swup';
 
 const navItems = document.querySelectorAll('.navigation .item');
+const navLinkList = document.querySelector('.navigation .links') as HTMLElement;
 
-swup.on('contentReplaced', () => {
+swup.on('animationInStart', () => {
   const currentPath = window.location.pathname;
-  navItems.forEach(item => {
+  navItems.forEach((item, index) => {
     const link = item.querySelector('a');
-    if (link) item.classList.toggle('active', new URL(link.href).pathname === currentPath);
+    if (link) {
+      const isLinkActive = new URL(link.href).pathname === currentPath;
+      item.classList.toggle('active', isLinkActive);
+      if (isLinkActive) navLinkList.style.setProperty('--activeIndex', index.toString());
+    }
   });
 });
