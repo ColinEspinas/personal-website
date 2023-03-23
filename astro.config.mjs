fd63db9f641809c61e42ab2e6f43b5af2eef4427
ColinEspinas <colin.espinas@yahoo.fr>
2023-03-23 12:23:18 +0100

    feat: added skip link and improved focus management

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,7 @@ import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://colinespinas.com',
+  site: 'https://colinespinas.com/',
   integrations: [tailwind({
     config: { applyBaseStyles: false }
   }), sitemap(), svelte(), mdx({
